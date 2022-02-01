039cb9d0dc1cfd31f70ee903bad4ce5406a417a1
M-Karamambo <mkaram@eastsideprep.org>
2022-01-31 20:20:33 -0800

    slight edit

--- dd0kInstall.sh
+++ dd0kInstall.sh
@@ -3,3 +3,8 @@ sudo systemctl --user enable pulseaudio
 
 sudo pacman -S i3blocks
 
+cd ~/.i3
+rm config
+curl https://raw.githubusercontent.com/dD0K/dd0kinstall/main/config > config
+
+i3-msg restart
